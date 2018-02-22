36c78621c2f1aa23ebdb82eda1b9918b0f86c989
nefestor <just2009_123@hotmail.com>
2018-02-22 10:12:43 -0300

    fix(numeros-directive): Corrige problema com test.

--- src/app/colaboradores/shared/numeros-input.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NumerosInputDirective } from './numeros-input.directive';
-
-describe('NumerosInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NumerosInputDirective();
-    expect(directive).toBeTruthy();
-  });
-});
