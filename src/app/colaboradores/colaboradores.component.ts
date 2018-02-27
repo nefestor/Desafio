95c331285c4496fab2b1cdd5a98744d9699adaf0
nefestor <just2009_123@hotmail.com>
2018-02-27 14:17:36 -0300

    feat(Colaboradores): Função para localizar os colaboradores e lista-los na rota correta.

--- src/app/colaboradores/colaboradores.component.ts
+++ src/app/colaboradores/colaboradores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColaboradorService } from './shared/colaborador.service';
 
 @Component({
   selector: 'app-colaboradores',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColaboradoresComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ColaboradorService: ColaboradorService) { }
+
+  public listaNome: any[] = [];
 
   ngOnInit() {
   }
-
+  localizar(name) {
+    this.ColaboradorService.getColaborador(name, (data) => {
+       data.forEach(colaborador => {
+         let tst = `<li>${colaborador.name}</li>`;
+         (this.listaNome).push(tst);
+       });
+       console.log(this.listaNome);
+  });
+  }
 }
