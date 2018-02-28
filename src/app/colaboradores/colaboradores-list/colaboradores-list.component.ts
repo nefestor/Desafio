370e945743f60b19745468edfa6a5ad3ce64fd55
nefestor <just2009_123@hotmail.com>
2018-02-28 15:12:14 -0300

    feat(colaboradores): Adiciona funcionalidade para excluir colaborador.

--- src/app/colaboradores/colaboradores-list/colaboradores-list.component.ts
+++ src/app/colaboradores/colaboradores-list/colaboradores-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ColaboradorService } from '../shared/colaborador.service';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-colaboradores-list',
@@ -12,27 +13,33 @@ export class ColaboradoresListComponent implements OnInit {
 
   subscriber: Subscription;
   
-  constructor(private ColaboradorService: ColaboradorService) { }
-
-  ngOnInit() {
-  }
+  constructor(private colaboradorService: ColaboradorService) { }
 
   public lista = [];
+  panelOpenState: boolean = false;
 
-  colaboradores = this.ColaboradorService.getColaboradores((data) => {
-    data.forEach(element => {
-      let tst = {
-        nome: element.name,
-        email: element.email
-      };
-      (this.lista).push(tst);
-    })
-    console.log(this.lista);
+  ngOnInit() {
+      this.atualizaLista();
+  }
+
+  atualizaLista() {
+    this.colaboradorService.getColaboradores((data) => {
+    this.lista = data;
   });
-
-
+  }
+  
   localiza(nome: string) {
     console.log(nome);
     let copiaLista: any[] = new Array;
+  }
+
+  excluir(id) {
+    this.subscriber = this.colaboradorService.excluirColaborador(id)
+        .subscribe(() => {
+          this.atualizaLista();
+        });
+  }
+  alterar(id, form) {
+    
   }
 }
