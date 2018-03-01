e356175a031f35fe41bfa4c3785964ad18ea82ea
nefestor <just2009_123@hotmail.com>
2018-03-01 15:08:36 -0300

    feat(core): Cria página inicial do projeto, listando ultimos projetos e colaboradores adicionados.

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ColaboradorService } from '../colaboradores/shared/colaborador.service';
+import { ProjetoService } from '../projetos/shared/projeto.service';
 
 @Component({
   selector: 'app-core',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoreComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private ColaboradorService: ColaboradorService, private ProjetoService: ProjetoService) { }
 
   ngOnInit() {
   }
 
+  lista = [];
+  listaProjeto = [];
+
+  listagem = this.ColaboradorService.getColaboradores((data) => {
+    data.forEach(element => {
+      let tst = {
+        nome: element.name,
+      };
+      (this.lista).push(tst);
+    })
+  });
+  
+  listagemProjeto = this.ProjetoService.getProjetos((data) => {
+    data.forEach(element => {
+      let tst = {
+        nome: element.name,
+      };
+      (this.listaProjeto).push(tst);
+    })
+  });
 }
